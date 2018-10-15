b218343912285ab15efb25c4adca9ad5e23474d9
yinxiupei <xiupeiyin@126.com>
2018-10-15 18:27:33 +0800

    Finish 326 and add 628

--- developing/326 - Power of Three.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {number} n
- * @return {boolean}
- */
-var isPowerOfThree = function (n) {
-  let result = Math.log(n) / Math.log(3)
-  return Number.isInteger(result)
-};
--- javascript/326 - Power of Three.js
+++ javascript/326 - Power of Three.js
@@ -1,6 +1,8 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
 var isPowerOfThree = function (n) {
   n = Math.log(n) / Math.log(3)
   return Math.abs(n - Math.round(n)) < 1e-10 ? true : false
 };
-let a = isPowerOfThree(0)
-console.log(a)
